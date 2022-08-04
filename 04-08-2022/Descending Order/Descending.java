cbf372aa6b88f482a52e632abb63d466c31a5149
Rutu204 <109073198+Rutu204@users.noreply.github.com>
2022-08-04 15:47:21 +0530

    Add files via upload

---
+++ 04-08-2022/Descending Order/Descending.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class Descending
+{
+	public static void main(String arg[])
+	{
+		int a[]= new int[5];
+		System.out.println("Enter Any 5 Numbers:");
+		Scanner s=new Scanner(System.in);
+		for(int i=0;i<a.length;i++)
+		{
+			a[i]=s.nextInt();
+		}
+		for(int i=0;i<a.length;i++)
+		{
+			for(int j=i+1;j<a.length;j++)
+			{
+				if(a[i]<a[j])
+				{
+					int no=a[i];
+					a[i]=a[j];
+					a[j]=no;
+				}
+			}
+		}
+		System.out.println("\nNumber in descending order:");
+		for(int i=0;i<a.length;i++)
+		{
+			System.out.println(a[i]);
+		}
+	}
+}
